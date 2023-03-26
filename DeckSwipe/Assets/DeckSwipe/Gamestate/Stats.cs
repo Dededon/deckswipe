fc0b31f320a75a7cfcebee3492805cac49b08365
Dededon <wowalvaroryan@gmail.com>
2023-03-25 21:16:06 -0500

    Minor Change

--- DeckSwipe/Assets/DeckSwipe/Gamestate/Stats.cs
+++ DeckSwipe/Assets/DeckSwipe/Gamestate/Stats.cs
@@ -12,6 +12,8 @@ namespace DeckSwipe.Gamestate {
 		private const int _startingFood = 16;
 		private const int _startingHealth = 16;
 		private const int _startingHope = 16;
+
+		private const int _true_end = 0;
 		
 		private static readonly List<StatsDisplay> _changeListeners = new List<StatsDisplay>();
 		
@@ -19,17 +21,22 @@ namespace DeckSwipe.Gamestate {
 		public static int Food { get; private set; }
 		public static int Health { get; private set; }
 		public static int Hope { get; private set; }
+		public static int true_end { get; private set; }
 		
 		public static float CoalPercentage => (float) Coal / _maxStatValue;
 		public static float FoodPercentage => (float) Food / _maxStatValue;
 		public static float HealthPercentage => (float) Health / _maxStatValue;
 		public static float HopePercentage => (float) Hope / _maxStatValue;
-		
+
+
 		public static void ApplyModification(StatsModification mod) {
 			Coal = ClampValue(Coal + mod.coal);
 			Food = ClampValue(Food + mod.food);
 			Health = ClampValue(Health + mod.health);
 			Hope = ClampValue(Hope + mod.hope);
+			/*
+			true_end = true_end + mod.true_end;
+			*/
 			TriggerAllListeners();
 		}
 		
@@ -43,6 +50,7 @@ namespace DeckSwipe.Gamestate {
 			Food = ClampValue(_startingFood);
 			Health = ClampValue(_startingHealth);
 			Hope = ClampValue(_startingHope);
+			true_end = _true_end;
 		}
 		
 		private static void TriggerAllListeners() {
